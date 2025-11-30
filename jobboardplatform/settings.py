b47ba591b194f2b5462d3eaf74f43b3e331606e9
Frank-ke254 <kahotadfrajeh@gmail.com>
2025-11-30 14:10:00 +0300

    feat: updated allowed hosts in settings

--- jobboardplatform/settings.py
+++ jobboardplatform/settings.py
@@ -134,10 +134,7 @@ WSGI_APPLICATION = 'jobboardplatform.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-        
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 SOCIALACCOUNT_PROVIDERS = {
@@ -185,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
