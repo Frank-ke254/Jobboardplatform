39f8a838ceba8de5dbfcc8622c911a75cb1c464a
Frank-ke254 <kahotadfrajeh@gmail.com>
2025-11-30 11:04:07 +0300

    feat: Update project settings

--- jobboardplatform/settings.py
+++ jobboardplatform/settings.py
@@ -64,6 +64,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.middleware.common.CommonMiddleware',
